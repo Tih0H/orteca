c75e55c7ce553575f102f4a76369a65ed2dd7f95
Tih0n <tihon_06@mail.ru>
2023-04-18 00:20:50 +0300

    перерыв

--- js/script.js
+++ js/script.js
@@ -12,7 +12,24 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 
-document.getElementById('button-order').onclick = function() {
+// const element = document.getElementsByClassName('product_buy');
+
+// element.addEventListener('click', function () {
+//   document.getElementById('modal-order').classList.add('modal_active');
+// });
+
+
+
+document.getElementById("button-order1").onclick = function() {
+  document.getElementById('modal-order').classList.add('modal_active');
+}
+document.getElementById("button-order2").onclick = function() {
+  document.getElementById('modal-order').classList.add('modal_active');
+}
+document.getElementById("button-order3").onclick = function() {
+  document.getElementById('modal-order').classList.add('modal_active');
+}
+document.getElementById("button-order4").onclick = function() {
   document.getElementById('modal-order').classList.add('modal_active');
 }
 document.getElementById('close-order_x').onclick = function() {
@@ -21,3 +38,42 @@ document.getElementById('close-order_x').onclick = function() {
 document.getElementById('close-order').onclick = function() {
   document.getElementById('modal-order').classList.remove('modal_active');
 }
+// document.getElementById('check').onkeydown = function (e) {
+//   return !(/^[А-Яа-яA-Za-z ]$/.test(e.key));  // IE > 9
+// }
+
+const form = document.getElementById("order-button");
+const inputs = form.elements;
+
+const registeredUsers = [
+  ["admin", "123456"],
+  ["comrade", "123456"],
+];
+
+function isValid(username, password) {   
+  for(var i = 0; i < registeredUsers.length; i++) {
+      var temp = registeredUsers[i];
+      if (temp[0] == username) {
+        var currentPair = temp;
+        break;        
+      }        
+  }     
+  if (currentPair && currentPair[1] == password) {
+    return true;           
+  } else {
+    return false;
+  }
+}
+
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  var username = inputs["login"].value;
+  var password = inputs["password"].value;  
+    if (!isValid(username, password)){ 
+      document.getElementById('submitButton').disabled = true;       
+      alert('Неверный логин или пароль');
+    } else {      
+      document.getElementById('submitButton').disabled = false;
+      alert('Верные данные');
+    }
+});
